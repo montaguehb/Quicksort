65adfdcd0946ffe4e432217a92c419f50064db19
Historia Montague <historia.montague@outlook.com>
2023-04-27 23:40:50 -0600

    removed a line

--- quicksort.js
+++ quicksort.js
@@ -1,15 +1,14 @@
 const unsorttedArr = [6, 5, 3, 7, 8, 2, 9, 1, 4]
 
-//sorts an array for all values between the indeces of lo and high
+//sorts an array for all values between the indeces of low and high
 //'splits' array into partitions
 //sorts each of the partitions
 function quickSort(arr, low, high) {
-    //returns if the hi and lo values are the same or if lo is greater
-    //because we will call this function recursively, this will happen once a partition is only 1 element 
+    //returns once there is only one element in a partition 
     if(low >= high || low < 0) {
         return
     }
-    //partitions the array and gives the index to pivot from
+    //partitions the array and gives the index of the "pivot"
     let p = partition(arr, low, high)
     //sorts to the left of the pivot
     quickSort(arr, low, p-1)
@@ -19,23 +18,22 @@ function quickSort(arr, low, high) {
 
 //Itterates over partitions of an array, moving values lower than the pivot to the left and higher values to the right
 function partition(arr, low, high) {
-    //sets the pivot to value of the highest item of the array
+    //sets the pivot value to the value at the highest index of the partition
     let pivot = arr[high]
 
     //sets a temp pivot index
     let i = low - 1
 
-    //iterates through the array between the lowest index and the pivot
+    // iterates between the low and high indices moving values lower than the
+    // pivot value to the left of the temporary pivot index
     for(let j = low; j < high; j++) {
-        //for each value, checks if it is lower than the value of the pivot, if it is; then move the temp index to the "right"/up one value and swaps the values. 
         if (arr[j] <= pivot) {
             i++;
             [arr[i], arr[j]] = [arr[j], arr[i]]
         }
     }
 
-    //Moves the pivot up and swaps the pivot with the value at the highest index
-    //returns the temp pivot
+    //inserts the pivot at the proper pivot index and returns the index
     i++;
     [arr[i], arr[high]] = [arr[high], arr[i]]
     return i
